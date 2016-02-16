d3d4dc8a339ae69e48d84a4ea308860f64848a5c
Jean Leno Heidemann <jeanleno@gmail.com>
2016-02-15 23:11:22 -0200

    Test Deploy

--- WebContent/app.js
+++ WebContent/app.js
@@ -1,4 +1,4 @@
 var app = angular.module('companyModule', [])
-        .constant('API_URL', 'http://localhost:8080/SpringRestService/api/companyservice/');
+        .constant('API_URL', 'https://spring-rest-jlheidemann.herokuapp.com/api/companyservice/');
 
         
